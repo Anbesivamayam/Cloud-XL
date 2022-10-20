1a687865aed536b6414316991a5d6ed6864e4ed4
ARUN PRASAD <arun.dot007@gmail.com>
2022-10-20 15:05:54 +0530

    Removed - predeploy: npm run build - deploy: gh-pages -d build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter,} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter  basename={window.location.pathname || ''}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename={window.location.pathname || ""}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>
 );
-
-
